5de97b6be1f53da9d68619cca98c026384a9d724
Beka Project <fordocs.tj@gmail.com>
2021-04-15 15:48:01 +0500

    About Us page added

--- src/js/script.js
+++ src/js/script.js
@@ -34,4 +34,36 @@ $(document).ready(function(){
             }
         }
     });
+
+    // SLICK SLIDER
+    $('.about-gallery-wrap').slick({
+        infinite: true,
+        arrows: false,
+        dots: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint:991,
+                settings:{
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint:767,
+                settings:{
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint:575,
+                settings:{
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            }
+        ]
+    });
 });
